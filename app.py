d9408ede06afef1d292c56b1d291976ad46c1fab
Saul Medina <125154086+saulbruh@users.noreply.github.com>
2025-03-19 20:08:03 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template,jsonify
+from flask_sqlalchemy import SQLAlchemy
+import mariadb
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, nombre, descripcion, precio, categoria, imagen, stock FROM productos")
+    productos = cursor.fetchall()
+    conn.close()
+
+    productos_json = [
+        {"id": p[0], "nombre": p[1], "descripcion": p[2], "precio": p[3], "categoria": p[4], "imagen": p[5], "stock": p[6]}
+        for p in productos
+    ]
+
+    return render_template('index.html', productos=productos_json)
+
+#Conexion a MariaDB sin ORM
+def get_db_connection():
+    return mariadb.connect(
+        user="railway",
+        password="Amdwzi-JlqxbQAzQFP~i-zZWK5L1J0P8",
+        host="switchyard.proxy.rlwy.net",
+        port=35476,
+        database="railway"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
